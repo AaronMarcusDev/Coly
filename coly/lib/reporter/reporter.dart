ae0b739b65b6f75979b32c2142c0c18a70447241
aaron marcus <aaronmarcus2007@gmail.com>
2023-03-26 00:21:44 +0100

    Experimental (improved) error messages

--- coly/lib/reporter/reporter.dart
+++ coly/lib/reporter/reporter.dart
@@ -2,8 +2,10 @@ import 'package:coly/shared/source/source.dart' as shared_source;
 
 class Reporter {
   void error(String file, int line, String message) {
-    print("\x1B[31m[ERROR] $file:$line $message\x1B[0m");
-    // print(shared_source.source.split("\n")[line-1]);
+    print("\x1B[31m[ERROR] $message\x1B[0m");
+    // print("\x1B[33m[TRACE]\x1B[0m $file:$line");
+    print("[TRACE]\x1B[0m $file:$line");
+    print("\x1B[33m   -->\x1B[0m  \x1B[1m\x1B[33m$line\x1B[0m \x1B[33m|\x1B[0m \x1b[0m${shared_source.source.split("\n")[line-1]}\n");
   }
 
   void warning(String file, int line, String message) {
